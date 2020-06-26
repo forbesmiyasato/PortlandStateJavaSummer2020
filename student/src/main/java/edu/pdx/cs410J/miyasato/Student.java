89dbfd3c22348e17860693ae78d61038dee5d71f
forbesmiyasato <miya5288@pacificu.edu>
2020-06-26 15:26:23 -0700

    updated Student, now displays expected output

--- student/src/main/java/edu/pdx/cs410J/miyasato/Student.java
+++ student/src/main/java/edu/pdx/cs410J/miyasato/Student.java
@@ -6,40 +6,76 @@ import java.util.ArrayList;
                                                                                     
 /**                                                                                 
  * This class is represents a <code>Student</code>.                                 
- */                                                                                 
-public class Student extends Human {                                                
-                                                                                    
-  /**                                                                               
-   * Creates a new <code>Student</code>                                             
-   *                                                                                
-   * @param name                                                                    
-   *        The student's name                                                      
-   * @param classes                                                                 
-   *        The names of the classes the student is taking.  A student              
-   *        may take zero or more classes.                                          
-   * @param gpa                                                                     
-   *        The student's grade point average                                       
-   * @param gender                                                                  
+ */
+public class Student extends Human {
+
+  private String pName;
+  private ArrayList<String> pClasses;
+  private double pGPA;
+  private String pGender;
+
+  /**
+   * Creates a new <code>Student</code>
+   *
+   * @param name
+   *        The student's name
+   * @param classes
+   *        The names of the classes the student is taking.  A student
+   *        may take zero or more classes.
+   * @param gpa
+   *        The student's grade point average
+   * @param gender
    *        The student's gender ("male" or "female", or "other", case insensitive)
-   */                                                                               
+   */
   public Student(String name, ArrayList<String> classes, double gpa, String gender) {
     super(name);
+    pName = name;
+    pClasses = classes;
+    pGPA = gpa;
+    pGender = gender;
   }
 
-  /**                                                                               
-   * All students say "This class is too much work"
+  /**
+   * All students say "\"This class is too much work\""
    */
   @Override
-  public String says() {                                                            
-    throw new UnsupportedOperationException("Not implemented yet");
+  public String says() {
+    return "\"This class is too much work\"";
   }
-                                                                                    
-  /**                                                                               
-   * Returns a <code>String</code> that describes this                              
-   * <code>Student</code>.                                                          
-   */                                                                               
+
+  /**
+   * Returns a <code>String</code> that describes this
+   * <code>Student</code>.
+   */
   public String toString() {
-    throw new UnsupportedOperationException("Not implemented yet");
+    String studentDescription = pName + " has a GPA of " + pGPA + " and is taking " + pClasses.size() + " classes: ";
+
+    for (int i = 0; i < pClasses.size(); i++) {
+      if (i == pClasses.size() - 1) {
+        studentDescription += pClasses.get(i) + ". ";
+      }
+      else {
+        studentDescription += pClasses.get(i) + ", ";
+      }
+    }
+
+    String whatTheySay = new String();
+
+    if (pGender.equals("male")) {
+      whatTheySay+="He";
+    }
+    else if (pGender.equals("female")) {
+      whatTheySay+="She";
+    }
+    else {
+      whatTheySay+="They";
+    }
+
+    whatTheySay = whatTheySay + " says " + this.says() + ".";
+
+    studentDescription += whatTheySay;
+
+    return studentDescription;
   }
 
   /**
@@ -48,7 +84,24 @@ public class Student extends Human {
    * standard out by invoking its <code>toString</code> method.
    */
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
+//    for (String string : args)
+//    {
+//      System.err.println(string);
+//    }
+
+    try {
+      ArrayList<String> classes = new ArrayList<String>();
+      for (int i = 3; i < args.length; i++) {
+        classes.add(args[i]);
+      }
+      Student cStudent = new Student(args[0], classes, Double.parseDouble(args[2]), args[1]);
+
+      System.out.println(cStudent.toString());
+    } catch (Exception e) {
+      System.err.println("Missing command line arguments");
+    }
+    finally {
+      System.exit(1);
+    }
   }
 }
