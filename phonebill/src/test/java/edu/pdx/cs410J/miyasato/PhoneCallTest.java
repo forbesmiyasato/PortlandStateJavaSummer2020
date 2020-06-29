3b84b9be1eeacb4c565acd47dd1305fa04dfec5c
forbesmiyasato <miya5288@pacificu.edu>
2020-06-29 00:37:22 -0700

    Added unit tests for PhoneCall, and all passed

--- phonebill/src/test/java/edu/pdx/cs410J/miyasato/PhoneCallTest.java
+++ phonebill/src/test/java/edu/pdx/cs410J/miyasato/PhoneCallTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for the {@link PhoneCall} class.
@@ -26,8 +27,42 @@ public class PhoneCallTest {
 
   @Test
   public void forProject1ItIsOkayIfGetStartTimeReturnsNull() {
-    PhoneCall call = new PhoneCall();
+    PhoneCall call = new PhoneCall("test", "test", null, "test");
     assertThat(call.getStartTime(), is(nullValue()));
   }
-  
+
+  @Test
+  public void checkIfGetCalleeGetsCorrectCallee () {
+    PhoneCall pc = createPhoneCall();
+
+    assertEquals(pc.getCallee(), "808-200-2000");
+  }
+
+  @Test
+  public void checkIfGetCallerGetsCorrectCaller () {
+    PhoneCall pc = createPhoneCall();
+
+    assertEquals(pc.getCaller(), "808-200-6188");
+  }
+
+  @Test
+  public void checkIfGetStartTimeStringGetsCorrectStartTime () {
+    PhoneCall pc = createPhoneCall();
+
+    assertEquals(pc.getStartTimeString(), "1/15/2020 19:39");
+  }
+
+  @Test
+  public void checkIfGetEndTimeStringGetsCorrectEndTime () {
+    PhoneCall pc = createPhoneCall();
+
+    assertEquals(pc.getEndTimeString(), "01/2/2020 1:03");
+  }
+
+  public PhoneCall createPhoneCall() {
+    PhoneCall pc = new PhoneCall("808-200-6188", "808-200-2000", "1/15/2020 19:39",
+            "01/2/2020 1:03");
+
+    return pc;
+  }
 }
