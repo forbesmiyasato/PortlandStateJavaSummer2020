c4b3c9afaa59379066464e3be48defbfcf57e0e7
forbesmiyasato <miya5288@pacificu.edu>
2020-07-07 15:25:07 -0700

    Added TextDumper and TextParser to Project 2 branch

---
+++ phonebill/src/main/java/edu/pdx/cs410J/miyasato/TextDumper.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.miyasato;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.PhoneBillDumper;
+
+import java.io.IOException;
+
+public class TextDumper implements PhoneBillDumper {
+    @Override
+    public void dump(AbstractPhoneBill abstractPhoneBill) throws IOException {
+
+    }
+}
---
+++ phonebill/src/main/java/edu/pdx/cs410J/miyasato/TextParser.java
@@ -0,0 +1,12 @@
+package edu.pdx.cs410J.miyasato;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.ParserException;
+import edu.pdx.cs410J.PhoneBillParser;
+
+public class TextParser implements PhoneBillParser {
+    @Override
+    public AbstractPhoneBill parse() throws ParserException {
+        return null;
+    }
+}
