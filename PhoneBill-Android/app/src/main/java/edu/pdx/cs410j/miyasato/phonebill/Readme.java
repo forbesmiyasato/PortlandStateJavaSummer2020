5ce670e445c18a1e76bbafeb687a669d72a5917a
forbesmiyasato <miya5288@pacificu.edu>
2020-08-06 01:16:34 -0700

    added add phone bill popup

---
+++ PhoneBill-Android/app/src/main/java/edu/pdx/cs410j/miyasato/phonebill/Readme.java
@@ -0,0 +1,15 @@
+package edu.pdx.cs410j.miyasato.phonebill;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Readme extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+  }
+}
